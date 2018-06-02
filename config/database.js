b1e938a95a3d08ad7e931c02b368d012e045a4a0
Carlos Jaramillo <cejaramillof@gmail.com>
2018-06-02 14:10:25 -0500

    xd

--- config/database.js
+++ config/database.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const dbName = 'places_facilito_api';
 
+const connectionString =
+  process.env.MONGODB_URI ||
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URI ||
+  "'mongodb://localhost/'+dbName";
+
 module.exports = {
-  connect: ()=> mongoose.connect('mongodb://localhost/'+dbName),
+  connect: ()=> mongoose.connect(connectionString),
   dbName,
   connection: ()=>{
     if(mongose.connection)
